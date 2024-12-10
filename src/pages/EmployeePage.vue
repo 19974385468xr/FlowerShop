1ba0f64b6367238fd5b75710875c49920aa88aee
19974385468xr <2844004186@qq.com>
2024-12-10 20:35:19 +0800

    初次提交

---
+++ src/pages/EmployeePage.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="q-pa-md">
+    <q-h3>员工</q-h3>
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input v-model="text" label="姓名" />
+    </div>
+</div>
+</template>
+
+<script lang='ts' setup>
+import {ref} from 'vue';
+const text = ref('');
+</script>
+
+<style>
+</style>
